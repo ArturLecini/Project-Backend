1b12f702a00a5f838da6b0eb470d967a1786c3f5
ArturLecini <artur.lecini98@gmail.com>
2020-11-08 07:14:30 +0100

    checkroles fixed

--- src/check/checkRole.ts
+++ src/check/checkRole.ts
@@ -6,19 +6,21 @@ import * as jwt from "jsonwebtoken";
 
 export const checkRole = (roles: Array<string>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
- const token = <string>req.header["token"];
+ const token = <string>req.headers["token"];
    const jwtPayload = <any>jwt.verify(token,config.jwtSecret);
    
     const userRepository = getRepository(USER);
     let user: USER;
     try {
       user = await userRepository.findOneOrFail({where:{EMAIL: jwtPayload.EMAIL}});
-    } catch (id) {
-      res.status(401).send();
+    } catch (e) {
+      res.status(401).send({message: 'user not have privilegies', success: false});
     }
 
     //Check if array of authorized roles includes the user's role
-    if (roles && roles.indexOf(user.ROLE) > -1) next();
-    else res.status(401).send({roles: roles ,USER:user, role: user.ROLE});
+    if (roles.length && !roles.includes(user.ROLE)){
+    res.status(401).send({roles: roles ,USER:user, role: user.ROLE});
+    }
+    else  next();
   };
 };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -9,15 +9,15 @@ import {USER} from "../entity/USER"
 const router = Router();
 
 
-//getb all users
-router.get('/' ,UserController.getAll);//,,checkRole(["ADMIN"]),[checkJwt, ][checkJwt],[checkJwt, ,[checkJwt, checkRole(["admin"])]checkRole(["ADMIN"])]
+//getb all users,[checkJwt,checkRole(["admin"])]
+router.get('/'
+ ,UserController.getAll);//,,checkRole(["ADMIN"]),[checkJwt, ][checkJwt],[checkJwt, ,[checkJwt, checkRole(["admin"])]checkRole(["ADMIN"])]
 
 //get one user only nr
 router.get('/:ID([0-9]+)',UserController.getById);//[checkJwt],
 
 //create new user
-router.post('/add' ,UserController.newUser);//,,[checkJwt],checkRole(["ADMIN"])
-
+router.post('/add' ,UserController.newUser);//,
 //edit user
 router.patch(`/edit/:ID([0-9]+)` , UserController.editUser);// ,[checkJwt]
 router.put('/edit/:ID', UserController.editUser);
